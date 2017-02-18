915510ca01a848dc3b66370fe438b754b4b3033d
vladimirStasic <stoske089@yahoo.com>
2017-02-18 12:17:53 +0100

    HTML/CSS - start

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {    
+
+	$('#menuButton').on('click',function myFunction() {
+	    var x = document.getElementById("myTopnav");
+	    if (x.className === "topnav") {
+	        x.className += " responsive";
+	    } else {
+	        x.className = "topnav";
+	    }
+	});
+	
+});
